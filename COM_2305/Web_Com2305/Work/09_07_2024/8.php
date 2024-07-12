bbeea114be156820b955e73d20628725595f5b31
pleuck11 <143922611+pleuck11@users.noreply.github.com>
2024-07-12 16:44:09 +0700

    whorkhome

---
+++ COM_2305/Web_Com2305/Work/09_07_2024/8.php
@@ -0,0 +1,13 @@
+<!-- 8. ให้เขียนโปรแกรมแสดงชื่อนักศึกษาจำนวน 5 ครั้งด้วยลูป for, while, do-while -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>แบบฝึกหัดข้อที่ 8</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ COM_2305/Web_Com2305/Work/09_07_2024/9_2.php
@@ -0,0 +1,14 @@
+<!-- 9.สร้างฟอร์มรับแม่สูตรคูณ จากนั้นให้แสดงแม่สูตรคูณ
+        9.1 ด้วยลูป while -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>แบบฝึกหัดข้อที่ 9.2</title>
+</head>
+<body>
+    
+</body>
+</html>
