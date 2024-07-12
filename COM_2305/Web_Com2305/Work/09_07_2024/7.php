99182d845e33e961ceb99a1b7226441a28dda5c0
pleuck11 <143922611+pleuck11@users.noreply.github.com>
2024-07-12 16:36:06 +0700

    Update 7.php

---
+++ COM_2305/Web_Com2305/Work/09_07_2024/7.php
@@ -0,0 +1,13 @@
+<!-- 7. สร้างฟอร์มรับตัวเลขและสัญลักษณ์คณิตศาสตร์ (+  -  *  / ) จากนั้นคำนวณตัวเลขตามสัญลักษณ์ที่เลือก ใช้คำสั่ง switch -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>แบบฝึกหัดข้อที่ 7</title>
+</head>
+<body>
+    
+</body>
+</html>
